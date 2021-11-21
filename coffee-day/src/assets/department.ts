101125d354b2023c400917232f13f9c798261f5e
Juferis <66511872+Juferis@users.noreply.github.com>
2021-11-21 17:53:16 +0900

    create mock data
    members, dep data

---
+++ coffee-day/src/assets/department.ts
@@ -0,0 +1,9 @@
+import { Department } from 'src/app/interface/member';
+export const department: Department[] = [
+  { department: '운영진' }, // 1
+  { department: '경영지원실' }, // 2
+  { department: '트윈 개발실' }, // 3
+  { department: '트윈 디자인실' }, // 4
+  { department: '가상공간연구 개발실' }, // 5
+  { department: '가상공간연구 디자인실' }, // 6
+];
---
+++ coffee-day/src/assets/mock.ts
@@ -0,0 +1,52 @@
+import { Member } from 'src/app/interface/member';
+import { department } from './department';
+
+export const ontwinsMember: Member[] = [
+  /**@운영진 */
+  {
+    department: '운영진',
+    name: '최상수',
+  },
+  {
+    department: '운영진',
+    name: '우정엽',
+  },
+  {
+    department: '운영진',
+    name: '송인호',
+  },
+  /**@경영지원실 */
+  {
+    department: '경영지원실',
+    name: '성유진',
+  },
+  {
+    department: '경영지원실',
+    name: '남강학',
+  },
+  /**@트윈_개발실 */
+  {
+    department: '트윈 개발실',
+    name: '이봉현',
+  },
+  {
+    department: '트윈 개발실',
+    name: '성백규',
+  },
+  {
+    department: '트윈 개발실',
+    name: '박준기',
+  },
+  {
+    department: '트윈 개발실',
+    name: '박혜란',
+  },
+  {
+    department: '트윈 개발실',
+    name: '강현진',
+  },
+  {
+    department: '트윈 개발실',
+    name: '이 솔',
+  },
+];
