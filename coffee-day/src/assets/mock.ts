92d36e87bf109641461c400d6c3ed2f3bca1ace7
Juferis <66511872+Juferis@users.noreply.github.com>
2021-11-21 17:58:03 +0900

    Update mock.ts

--- coffee-day/src/assets/mock.ts
+++ coffee-day/src/assets/mock.ts
@@ -49,4 +49,63 @@ export const ontwinsMember: Member[] = [
     department: '트윈 개발실',
     name: '이 솔',
   },
+  /**@트윈_디자인실 */
+  {
+    department: '트윈 디자인실',
+    name: '서은희',
+  },
+  {
+    department: '트윈 디자인실',
+    name: '윤경진',
+  },
+  {
+    department: '트윈 디자인실',
+    name: '김재하',
+  },
+  {
+    department: '트윈 디자인실',
+    name: '길희라',
+  },
+  {
+    department: '트윈 디자인실',
+    name: '조윤정',
+  },
+  {
+    department: '트윈 디자인실',
+    name: '김영우',
+  },
+  {
+    department: '트윈 디자인실',
+    name: '이힘찬',
+  },
+  /**@가상공간연구_개발실 */
+  {
+    department: '가상공간연구 개발실',
+    name: '윤가중',
+  },
+  {
+    department: '가상공간연구 개발실',
+    name: '유진탁',
+  },
+  /**@가상공간연구_디자인 */
+  {
+    department: '가상공간연구 디자인실',
+    name: '김미애',
+  },
+  {
+    department: '가상공간연구 디자인실',
+    name: '이동원',
+  },
+  {
+    department: '가상공간연구 디자인실',
+    name: '김석헌',
+  },
+  {
+    department: '가상공간연구 디자인실',
+    name: '정수현',
+  },
+  {
+    department: '가상공간연구 디자인실',
+    name: '이선화',
+  },
 ];
