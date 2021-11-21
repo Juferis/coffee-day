3d47f78cf3cba581c6877bf353c66f870f92bec1
Juferis <66511872+Juferis@users.noreply.github.com>
2021-11-21 16:46:49 +0900

    create title
    타이틀 생성

--- coffee-day/src/app/home/homepage/homepage.component.ts
+++ coffee-day/src/app/home/homepage/homepage.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  public on = 'On';
+  public twins = 'twins';
+  public and = ' & ';
+  public paranoid = 'Paranoid';
+  constructor() {}
 
   ngOnInit(): void {
+    const userCheck = localStorage.getItem('user');
+    if (!userCheck) {
+      console.log('our user!');
+    }
   }
-
 }
